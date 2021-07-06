61ca8bd255c545f1809dc3f6abec9f7eff63b99d
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-07-06 07:31:41 +0700

    Initial commit

---
+++ src/main/java/com/nyash/travellizer/travellizertripservice/TravellizerTripServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nyash.travellizer.travellizertripservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravellizerTripServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravellizerTripServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nyash/travellizer/travellizertripservice/TravellizerTripServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nyash.travellizer.travellizertripservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravellizerTripServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
